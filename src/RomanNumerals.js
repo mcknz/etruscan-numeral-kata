cc02d73333772dfe56dba8386254f7bef2b35048
Jeff McKenzie <mail@mcknz.com>
2011-08-26 09:52:06 -0400

    minimore

--- src/RomanNumerals.js
+++ src/RomanNumerals.js
@@ -1,19 +1,15 @@
-//Number.prototype.toRomanNumeral = o
-function r(d) {
-    var b='',c=2,m=1e3;
-    function i(n, d, e){
-        var y=[9,5,4,1],z=[['IX','V','IV','I'],['XC','L','XL','X'],['CM','D','CD','C']],c=0,p=Math.pow(10,e),x,f;
-        e < 0 ? e = 0 : e = e;
-        while (n >= y[3] * p) {
-            x = (y[c] * p);
+function r(d){var b='',m=1e3;function i(n,d,e){
+y=[9,5,4,1],z=[['IX','V','IV','I'],['XC','L','XL','X'],['CM','D','CD','C']],c=0,p=Math.pow(10,e);
+        e=e<0?0:e;
+        while(n>=y[3]*p) {
+            x=(y[c]*p);
             f=n>=x;
-            d = f?d + z[e][c]:d;
-            n = f?n-x:n;
-            c = f?c:c+1;
-            
+            d=f?d+z[e][c]:d;
+            n=f?n-x:n;
+            c=f?c:c+1;
         }
-        return n == 0 ? d : i(n, d, --e);
+        return n==0?d:i(n,d,--e);
     }
-    while (d >= m) { b += 'M'; d -= m; }
-    return i(d,b,c);
+    while(d>=m){b+='M';d-=m};
+    return i(d,b,2)
 }
