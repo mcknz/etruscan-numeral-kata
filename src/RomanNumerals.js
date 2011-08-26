b815f6388db69b1820068803dfac796d5f1d8d8a
Jeff McKenzie <mail@mcknz.com>
2011-08-26 10:03:58 -0400

    no mas

--- src/RomanNumerals.js
+++ src/RomanNumerals.js
@@ -1,15 +1,1 @@
-function r(d){var b='',m=1e3;function i(n,d,e){
-y=[9,5,4,1],z=[['IX','V','IV','I'],['XC','L','XL','X'],['CM','D','CD','C']],c=0,p=Math.pow(10,e);
-        e=e<0?0:e;
-        while(n>=y[3]*p) {
-            x=(y[c]*p);
-            f=n>=x;
-            d=f?d+z[e][c]:d;
-            n=f?n-x:n;
-            c=f?c:c+1;
-        }
-        return n==0?d:i(n,d,--e);
-    }
-    while(d>=m){b+='M';d-=m};
-    return i(d,b,2)
-}
+function r(n){function e(n,b,d){y=[9,5,4,1],z=[["IX","V","IV","I"],["XC","L","XL","X"],["CM","D","CD","C"]],c=0,p=Math.pow(10,d),d=d<0?0:d;while(n>=y[3]*p)x=y[c]*p,f=n>=x,b=f?b+z[d][c]:b,n=f?n-x:n,c=f?c:c+1;return n==0?b:e(n,b,--d)}var b="",d=1e3;while(n>=d)b+="M",n-=d;return e(n,b,2)}
